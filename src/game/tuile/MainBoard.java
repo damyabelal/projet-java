465be4a631b5fb08ff04ed24424a7b6767f5ac18
fatima.almohamed-alsadou.etu <your_email@example.com>
2025-01-25 14:57:00 +0100

    amelioration methode createBoard et creation fichier mainBoard qui prend des arguments

---
+++ src/game/tuile/MainBoard.java
@@ -0,0 +1,19 @@
+package game.tuile;
+import java.util.*;
+import game.util.*;
+import java.lang.Integer;
+
+public class MainBoard{
+  public static void  main(String[] args ){
+    try {
+    int height = Integer.parseUnsignedInt(args[0]);
+    int width = Integer.parseUnsignedInt(args[1]);
+    Board b= new Board(height,width);
+    b.createBoard();
+    b.display();
+    
+} catch (NumberFormatException e) {
+    System.out.println("Input height and width.");
+}
+  }
+}
