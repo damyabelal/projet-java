617a82aa25dcb8bcb35510a0e7de338ecd284285
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-01-28 19:44:40 +0100

    traduction

---
+++ src/game/tuile/Earth.java
@@ -0,0 +1,39 @@
+package game.tuile;
+
+public class Terrestre extends Tuile{
+
+   protected Ressource ressource ;
+   protected String symbol ;
+   
+   /** initialises  a new earth tile of the given type*/
+   public Terrestre(Ressource ressource , String symbol){
+    super(symbol);
+    this.ressource=ressource;
+    this.symbol =symbol;
+    }
+
+/** returns the ressources of this tile
+ * @return string which is the type of this tile
+ */
+    public Ressource getRessource(){
+        return this.ressource;
+    }
+
+
+
+ /** returs true if tis tile has a building , False otherwise
+ * @return true of this tile has a building 
+
+public boolean haveBuild(){}
+
+*/
+
+
+
+
+/** adds a new building to this tile */
+    public void addBuilding(){
+}
+
+    
+}
