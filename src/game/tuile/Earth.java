1887b8c9c75002c6ec8c87ab515624609b12853e
fatima.almohamed-alsadou.etu <your_email@example.com>
2025-02-08 11:39:01 +0100

    ajout de l'attribut quantiteRessources a la classe earth et le getter qui vas avec. Creation egalement de la methode addRessource()

--- src/game/tuile/Earth.java
+++ src/game/tuile/Earth.java
@@ -4,6 +4,8 @@ public class Earth extends Tuile{
 
    protected Ressource ressource ;
    protected String symbol ;
+   protected int quantiteRessource;// j'ai met cet attribut car chaque tuile a une quantité de ressource qui 
+                                    // egale a 0 au depart mais qui augmente a chaque tour de jeu
    
    /** initialises  a new earth tile of the given type
     * @param ressource the ressource
@@ -13,13 +15,24 @@ public class Earth extends Tuile{
     super(symbol);
     this.ressource=ressource;
     this.symbol =symbol;
+    this.quantiteRessource=0; // quand on construit une tuile elle possede au depart 0 ressouces
+
     }
 
 /** returns the ressources of this tile
  * @return string which is the type of this tile
  */
     public Ressource getRessource(){
-        return this.ressource;
+        return this.quantiteRessource;
+    }
+
+/** adds a number of the tile's  ressources to the already existing quantity of ressources
+ * 
+  */
+    public void addRessource(int nbre){
+        this.quantiteRessource+=nbre; // on a besoind de cette methode pour augmenter le nombre de 
+                                        // ressource de chaque tuile a chaque tour de jeu
+        
     }
 
 
@@ -37,7 +50,16 @@ public boolean haveBuild(){}
 /** adds a new  building to this tile with the given capacity  
  * @param capacity the capacity of the building
 */
-    public void addBuilding(int capacity){};
+    public void addBuilding(int capacity){
+        
+
+
+    };
+
+
+     public boolean haveEnoughRessources(){
+        //TO DO
+    }
 
     
 }
