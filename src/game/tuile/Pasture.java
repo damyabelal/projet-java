2197023aedcb37462cf8635604a4c57302f2f2f0
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-02-10 13:13:59 +0100

    mise des emojis en commentaire

--- src/game/tuile/Pasture.java
+++ src/game/tuile/Pasture.java
@@ -2,7 +2,8 @@ package game.tuile;
 
 
 public class Pasture extends Earth{
-   private static final String SYMBOL = " 🐏 ";
+   /**private static final String SYMBOL = " 🐏 "; */
+   private static final String SYMBOL = " P ";
 /** initialise a new tile of the type  pasture */
 
  public Pasture(){
