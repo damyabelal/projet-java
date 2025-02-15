7f492a42422639a9a9a89b8b0f450dfd73059857
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-02-15 23:16:52 +0100

    ajout du constructeur cost pour le port

--- src/game/tuile/Port.java
+++ src/game/tuile/Port.java
@@ -3,11 +3,15 @@ package game.tuile;
 import java.util.HashMap;
 
 public class Port extends Building{
+    
+    private static final String SYMBOL = " -> ";        //" ⚓ "
 
-    public Port(Earth tuile, int dimension,HashMap<Integer, Ressource> cost){
+    public Port(Earth tuile){
         super(tuile);
-        this.dimension = dimension;
+        this.symbol = SYMBOL;
         this.cost= new HashMap<>(); 
+        this.cost.put(Ressource.WOOD,1);
+        this.cost.put(Ressource.SHEEP,2);
     }
 
     
