418d2948371ba039772d536aa7914448c3efa1de
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-02-15 14:35:59 +0100

    initialisation des classes

---
+++ src/game/tuile/Farm.java
@@ -0,0 +1,15 @@
+package game.tuile;
+
+public class Farm extends Building{
+
+    public Farm(Earth tuile) {
+        super(tuile);
+    }
+
+    @Override
+    protected boolean canBuild() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'canBuild'");
+    }
+    
+}
--- src/game/tuile/Port.java
+++ src/game/tuile/Port.java
@@ -1,8 +1,21 @@
 package game.tuile;
-public class Port{
 
+import java.util.HashMap;
+
+public class Port extends Building{
+
+    private Tuile tuile;
+    private int dimension;
+    private String symbol;
+    private HashMap<Integer,Ressource> cost;
 
     public Port(){
-        
+        super(tuile, dimension, symbol, cost);
+    }
+
+    @Override
+    protected boolean canBuild() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'canBuild'");
     }
 }
