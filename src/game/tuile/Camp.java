044df82b128320f301f0b73f408a5050abfe4fcf
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-02-15 14:28:39 +0100

    modifications + ajout de nouvelles classes

--- src/game/tuile/Camp.java
+++ src/game/tuile/Camp.java
@@ -2,9 +2,8 @@ package game.tuile;
 
 public class Camp extends Army{
 
-    public Camp(int nbWarriors, Earth tuile, int capacity){
-        super(nbWarriors, tuile, capacity);
+    public Camp(){
+        super(tuile, nbWarriors);
     }
-
 
 }
