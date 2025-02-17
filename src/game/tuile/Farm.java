e1bb5e1016fbca51b853d8c07a256f2a6a4f1da0
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-02-17 16:37:05 +0100

    classe farm et exploitation complète

--- src/game/tuile/Farm.java
+++ src/game/tuile/Farm.java
@@ -2,9 +2,12 @@ package game.tuile;
 
 public class Farm extends Building{
 
+    /** Build a Farm
+     * @param tuile the tile where we build the farm
+     */
     public Farm(Earth tuile) {
         super(tuile);
+        this.dimension = 1;
     }
 
-    
 }
