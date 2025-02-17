638a9a9b05f8dd91cdfed93fc362f20e7a56a0df
fatima.almohamed-alsadou.etu <your_email@example.com>
2025-02-17 15:39:33 +0100

    ajout de methode de test dans CampTest pour addAdditionnalWarriors()

--- test/game/tuile/CampTest.java
+++ test/game/tuile/CampTest.java
@@ -1,11 +1,24 @@
 package game.tuile;
 
-import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class CampTest{
+
+@Test 
+void GetAdditionnalWarriorTest(){
+  Ressource ressource= Ressource.SHEEP;
+  Earth tuile= new Earth(ressource,"F");
+  Camp camp = new Camp(tuile ,8);
+  assertEquals(camp.getAdditionalWarriors(),3);
+
+}
+
+
 
 
 }
