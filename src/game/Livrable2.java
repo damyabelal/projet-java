e24f1d0a8f58a317d3d8b47d8e0d89d1d48e9c91
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-02-18 23:10:49 +0100

    associer les buildings a leur tuile dans le main

--- src/game/Livrable2.java
+++ src/game/Livrable2.java
@@ -10,39 +10,37 @@ public class Livrable2 {
         int width = Integer.parseInt(args[0]);
         int height = Integer.parseInt(args[1]);
 
-        if (width <= 10 || height <= 10) {
+        if (width < 10 || height < 10) {
             System.out.println("minimum dimensions must be 10");
             return;
         }
-
         Board board = new Board(width, height);
-        board.display(); // affichage au debut du jeu
-        /// place buidings 
+        board.display(); 
         placeBuildings(board);
 
     }
     private static void placeBuildings(Board board){
+
         Player player = new Player("player1");
-       
+
         Earth portTile = new Forest();
         Earth farmTile = new Field();
         Earth exploitationTile = new Pasture();
         Earth armyTile = new Mountain();
         Earth campTile = new Mountain();                        
 
-
-
-
-
         Port port = new Port(portTile);
         Farm farm = new Farm(farmTile);
         Exploitation exploitation = new Exploitation(exploitationTile);
         Army army = new Army(armyTile,5);
         Camp camp = new Camp(campTile,20);
-
-        /// y a un truc que je ne sais pas si je dosi les placer sur le board ou pas et comment le faire 
-
-
+ 
+        // associer les building a leur tuile 
+        portTile.setBuilding(port);
+        farmTile.setBuilding(farm);
+        exploitationTile.setBuilding(exploitation);
+        armyTile.setBuilding(army);
+        campTile.setBuilding(camp);
 
         /// display costs
         port.displayCost();
@@ -51,6 +49,7 @@ public class Livrable2 {
         army.displayCost();
         camp.displayCost();
 
+        System.out.println("--------------------------------------");
 
         ///display collected resources 
         port.collectRessource(player);
