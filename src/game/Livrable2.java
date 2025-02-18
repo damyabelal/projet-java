f76cb262024adabbe2ffdd8b2e740b2e8e75fe41
CHALAH <laeticia.chalah.etu@univ-lille.fr>
2025-02-18 12:42:08 +0100

    Progression sur le livrable 2

--- src/game/Livrable2.java
+++ src/game/Livrable2.java
@@ -1,7 +1,8 @@
 package game;
+import game.tuile.*;
 
 public class Livrable2 {
-    public static void main (string[] args){
+    public static void main (String[] args){
         if (args.length < 2) {
             System.out.println("you have to give two positive setting");
             return;
@@ -16,16 +17,47 @@ public class Livrable2 {
 
         Board board = new Board(width, height);
         board.display(); // affichage au debut du jeu
+        /// place buidings 
+        placeBuildings(board);
 
-        placeBuilding(board);
+    }
+    private static void placeBuildings(Board board){
+        Player player = new Player("player1");
+       
+        Earth portTile = new Forest();
+        Earth farmTile = new Field();
+        Earth exploitationTile = new Pasture();
+        Earth armyTile = new Mountain();
+        Earth campTile = new Mountain();                        
 
 
-        private void placeBuilding(Board board){
 
 
-        }
 
-        
+        Port port = new Port(portTile);
+        Farm farm = new Farm(farmTile);
+        Exploitation exploitation = new Exploitation(exploitationTile);
+        Army army = new Army(armyTile,5);
+        Camp camp = new Camp(campTile,20);
+
+        /// y a un truc que je ne sais pas si je dosi les placer sur le board ou pas et comment le faire
+
+
+
+        /// display costs
+        port.displayCost();
+        farm.displayCost();
+        exploitation.displayCost();
+        army.displayCost();
+        camp.displayCost();
+
+
+        ///display collected resources 
+        port.collectRessource(player);
+        farm.collectRessource(player);
+        exploitation.collectRessource(player);
+        army.collectRessource(player);
+        camp.collectRessource(player);
 
 
 
