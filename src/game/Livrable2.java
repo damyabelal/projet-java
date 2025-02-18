1ef88b114c955e18dabef6d50d28bfb35cd18a8f
CHALAH <laeticia.chalah.etu@univ-lille.fr>
2025-02-18 12:09:03 +0100

    Progression sur le livrable 2

--- src/game/Livrable2.java
+++ src/game/Livrable2.java
@@ -1,5 +1,36 @@
 package game;
 
 public class Livrable2 {
+    public static void main (string[] args){
+        if (args.length < 2) {
+            System.out.println("you have to give two positive setting");
+            return;
+        }
+        int width = Integer.parseInt(args[0]);
+        int height = Integer.parseInt(args[1]);
+
+        if (width <= 10 || height <= 10) {
+            System.out.println("minimum dimensions must be 10");
+            return;
+        }
+
+        Board board = new Board(width, height);
+        board.display(); // affichage au debut du jeu
+
+        placeBuilding(board);
+
+
+        private void placeBuilding(Board board){
+
+
+        }
+
+        
+
+
+
+
+    }
+
     
 }
