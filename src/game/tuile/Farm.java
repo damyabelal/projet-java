0d3939433d5e08c9dae1f3db9216ddccb3d6acd3
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-02-18 11:12:33 +0100

    evolution de farm en exploitation

--- src/game/tuile/Farm.java
+++ src/game/tuile/Farm.java
@@ -1,4 +1,6 @@
 package game.tuile;
+import java.util.HashMap;
+import game.Player;
 
 public class Farm extends Building{
 
@@ -12,6 +14,27 @@ public class Farm extends Building{
         this.cost.put(Ressource.WOOD, 1);
         this.cost.put(Ressource.ORE, 1);
     }
-    
 
+    /**
+     * return true if the farm can be a exploitation
+     * @return boolean
+     */
+    public boolean canBeExploitation(Player player){
+        return player.hasEnoughRessources(this);
+    }
+    
+    /** evolve the farm into a exploitation
+    * @param player the player who wants to upgrade the farm
+    * @return the new exploitation if the farm can be upgraded null otherwise
+    */
+    public Exploitation upGradeToExploitation(Player player) {
+        if (this.canBeExploitation(player)) {
+            Exploitation exploitation = new Exploitation(this.getTuile());
+            System.out.println("Farm evolve into exploitation");
+            return exploitation;
+        } else {
+            System.out.println("Not enough resources");
+        }
+        return null;
+    }
 }
