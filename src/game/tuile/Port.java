1f0fa8c8d62dd628f77dea55f3db1530f1f23164
CHALAH <laeticia.chalah.etu@univ-lille.fr>
2025-02-18 11:25:39 +0100

    ajout des methodes qui verifient si on peut placer un port ou non

--- src/game/tuile/Port.java
+++ src/game/tuile/Port.java
@@ -1,5 +1,6 @@
 package game.tuile;
-
+import game.Board;
+import game.util.*;
 import java.util.HashMap;
 
 public class Port extends Building{
@@ -13,6 +14,51 @@ public class Port extends Building{
         this.cost.put(Ressource.WOOD,1);
         this.cost.put(Ressource.SHEEP,2);
     }
+    /**
+     * return the number of sea tiles around the port
+     * @param pos
+     * @param board
+     * @return int the number of sea tiles around the port
+     */
+    private int nbSeaTiles(Position pos, Board board) {
+        int nbSeaTiles = 0;
+        for (Direction d : Direction.values()) {
+            Position neighbor = pos.next(d);
+            if (board.isValidPosition(neighbor)) {
+                Tuile neighborTile = board.getTile(neighbor);
+                if (neighborTile instanceof Sea) {
+                    nbSeaTiles += 1;
+                }
+            }
+        }
+        return nbSeaTiles;
+    }
+    /**
+     * return true if the port can be placed at the given position , false otherwise
+     * @param pos
+     * @param board
+     * @return boolean true if the port can be placed at the given position , false otherwise
+     */
+    public boolean canPlacePort(Position pos, Board board) {
+        if (!(board.getTile(pos) instanceof Earth)) {
+            return false;
+        }
+        for (Direction d : Direction.values()) {
+            Position neighbor = pos.next(d);
+            Tuile neighborTile = board.getTile(neighbor);
+            if (neighborTile instanceof Sea && nbSeaTiles(pos, board) >= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+   
+    }
+
+
 
     
 }
