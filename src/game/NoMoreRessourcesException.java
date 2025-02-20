952003e8178aa58631901fe78f67dff64123454d
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-02-20 19:17:34 +0100

    creation d'une exception

---
+++ src/game/NoMoreRessourcesException.java
@@ -0,0 +1,14 @@
+package game;
+
+public class NoMoreRessourcesException extends Exception {
+
+	public NoMoreRessourcesException() {
+		super();
+	}
+
+	public NoMoreRessourcesException(String arg0) {
+		super(arg0);
+	}
+
+
+}
