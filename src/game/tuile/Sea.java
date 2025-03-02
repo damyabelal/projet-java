f687893d0fdc8d01ac39935c783385365269ffbc
fatima.almohamed-alsadou.etu <your_email@example.com>
2025-03-02 17:22:26 +0100

    ajout cost arme secrete

--- src/game/tuile/Sea.java
+++ src/game/tuile/Sea.java
@@ -1,6 +1,7 @@
 package game.tuile;
 
 public class Sea extends Tuile{
+    public static final String BLUE = "\u001B[34m"; 
     private static final String SYMBOL = " S ";
     /** initiales a new tile of type sea */
     public Sea(){
