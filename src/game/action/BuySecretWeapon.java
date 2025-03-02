7b1c670e90f83c126fbd88b3803927dd8c6be463
fatima.almohamed-alsadou.etu <your_email@example.com>
2025-03-02 17:26:09 +0100

    ajout methode canBuy dans buyarmesecrete

--- src/game/action/BuySecretWeapon.java
+++ src/game/action/BuySecretWeapon.java
@@ -1,5 +1,23 @@
 package game.action;
 
+import java.util.HashMap;
+
+import game.tuile.Ressource;
+
 public class BuySecretWeapon implements Action{
+
+  public HashMap<Ressource,Integer> cost=new HashMap<>();
+  cost.put(Ressource.ORE);
+  cost.put(Ressource.WODD);
+
+  public boolean canBuy(HashMap<Ressource, Integer> inventory) {
+        for (Ressource ressource: cost.key()){
+            if (ressource.getOrDefault(ressource,0)< cost.get(ressource)){
+                return false;
+            }
+        }
+        return true;
+    }
+
     
 }
