a2d8cd120e018ea267cca38e808002cf5cd8f715
Almohamed Alsadou Fatima <fatima.almohamed-alsadou.etu@a16p13.fil.univ-lille.fr>
2025-03-03 11:21:50 +0100

    ajout de attribut warrior dans player

---
+++ src/game/PlayerAres.java
@@ -0,0 +1,13 @@
+package game;
+
+
+import java.util.*;
+import game.tuile.building.*;
+import game.util.*;
+import game.tuile.*;
+
+public class  PlayerAres{
+
+        private int warriors;
+        
+}
