d61e950bed223d46c1e9d3738a0aef621a954e85
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-03-04 23:41:47 +0100

    creation de l'action buildArmy

--- src/game/action/BuildArmy.java
+++ src/game/action/BuildArmy.java
@@ -2,13 +2,29 @@ package game.action;
 
 import game.NoMoreRessourcesException;
 import game.PlayerAres;
+import game.tuile.Ressource;
+import game.tuile.building.Army;
 
-public class BuildArmy implements Action<PlayerAres>{
+public class BuildArmy implements Action<PlayerAres> {
 
     @Override
     public void act(PlayerAres player) throws NoMoreRessourcesException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'act'");
+
+        if (player.getWarriors() < 1) {
+            throw new NoMoreRessourcesException("You need at least 1 warrior to build an Army");
+        }
+
+        if (player.getRessourceAmount(Ressource.WOOD) < 1 ||
+            player.getRessourceAmount(Ressource.SHEEP) < 1 ||
+            player.getRessourceAmount(Ressource.WEALTH) < 1) {
+            throw new NoMoreRessourcesException("Not enough resources to build an Army");
+        }
+
+        player.removeRessource(Ressource.WOOD, 1);
+        player.removeRessource(Ressource.SHEEP, 1);
+        player.removeRessource(Ressource.WEALTH, 1);
+
+        player.addArmy(new Army(null, 0));
+
     }
-    
 }
