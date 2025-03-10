51df5d346ce83a8431ab2495719afa564f30c2c2
Belal Damya <damya.belal.etu@a16p15.fil.univ-lille.fr>
2025-03-10 12:15:13 +0100

    update

--- src/game/action/BuildArmy.java
+++ src/game/action/BuildArmy.java
@@ -11,7 +11,6 @@ import java.util.List;
 import game.Board;
 import game.tuile.Earth;
 
-<<<<<<< HEAD
 public class BuildArmy extends ActionManager implements Action<PlayerAres> {
 
     public BuildArmy(){
@@ -20,43 +19,6 @@ public class BuildArmy extends ActionManager implements Action<PlayerAres> {
         this.cost.put(Ressource.SHEEP, 1);
         this.cost.put(Ressource.WEALTH, 1);
     }
-=======
-public class BuildArmy implements Action<PlayerAres> {
-    public Board board;
-    public BuildArmy(Board board) {
-        this.board = board;
-    }
-
-    /**
-     * Check if the player can build an army if in the island there are at least 2 buildings and 1 port
-     * @param earth
-     * @param player
-     * @return boolean
-     */
-    private boolean canBuildArmy(Earth earth, PlayerAres player) {
-        int cptBuild = 0;
-        int cptPort = 0;
-        List<Earth> island = board.getIsland(earth);
-        for (Earth tuile : island){
-            if(tuile.haveBuild()){
-
-                cptBuild++;
-            }
-            if(tuile.getBuilding() instanceof Port){
-                cptPort++;
-            }
-
-        }
-        return cptBuild >=2 && cptPort >= 1 && player.hasEnoughRessources(earth.getBuilding()) ;
-    }
-        
-   
-        
-
-
-
-
->>>>>>> 92a6866a6eef210a880b84d4decfee138ffa8459
 
     @Override
     public void act(PlayerAres player) throws NoMoreRessourcesException {
