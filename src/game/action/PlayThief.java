e631034d7c8940d77ffb575ae6637a6f00f786c8
Almohamed Alsadou Fatima <fatima.almohamed-alsadou.etu@a16p13.fil.univ-lille.fr>
2025-03-10 11:33:26 +0100

    debut methode steal

--- src/game/action/PlayThief.java
+++ src/game/action/PlayThief.java
@@ -6,20 +6,21 @@ import game.tuile.Ressource;
 
 public class PlayThief implements Action<PlayerDemeter>{
 
-
-  /** steals the Ressource ressource from every player and adds them to the given  player's inventory 
+    private Ressource ressource; // le type de ressource que this player veut voler
+  
+  
+    /** steals the Ressource ressource from every player and adds them to the given  player's inventory 
    * @param ressource the type of ressource that the thief will steal and give to the given player
   */
-  public void steal(Ressource ressource, PlayerDemeter player ){
-    // il faut trouver un moyen de recuperer tout les joueurs du plateau pour pouvoir 
-    //accéder a leur ressources
-
-  }
-    
   @Override
-  public void act(PlayerDemeter player) throws NoMoreRessourcesException {
+  public void act(PlayerDemeter[] player) throws NoMoreRessourcesException {
     // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'act'");
+    throw new UnsupportedOperationException("Unimplemented method 'act'"); 
+    for (int i=0 ; i<player.length ;i++){
+      
+
+
+    }
   }
 
 }
