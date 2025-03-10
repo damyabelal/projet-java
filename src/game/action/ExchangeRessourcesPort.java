90cead4e43d913aed6d2dc6897672a11e2b12140
Almohamed Alsadou Fatima <fatima.almohamed-alsadou.etu@a16p13.fil.univ-lille.fr>
2025-03-10 11:59:44 +0100

    ajout attribut des ressouces a echanger et creation de constructeur dans exchangeressourcesport

--- src/game/action/ExchangeRessourcesPort.java
+++ src/game/action/ExchangeRessourcesPort.java
@@ -4,11 +4,12 @@ import game.NoMoreRessourcesException;
 import game.PlayerDemeter;
 
 public class ExchangeRessourcesPort implements Action<PlayerDemeter>{
-
+    // permet d'échanger de ressources identiques identiques contre une autre
     @Override
     public void act(PlayerDemeter player1 ,PlayerDemeter player2) throws NoMoreRessourcesException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'act'");
+    
+
+
     }
     
 }
