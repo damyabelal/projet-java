29790ab9352e907641be794a7e3e1c10b6c0d51e
Almohamed Alsadou Fatima <fatima.almohamed-alsadou.etu@a16p13.fil.univ-lille.fr>
2025-03-10 12:05:07 +0100

    fin methode act dans exchange ressource port

--- src/game/action/ExchangeRessourcesPort.java
+++ src/game/action/ExchangeRessourcesPort.java
@@ -2,11 +2,29 @@ package game.action;
 
 import game.NoMoreRessourcesException;
 import game.PlayerDemeter;
+import game.tuile.Ressource;
 
 public class ExchangeRessourcesPort implements Action<PlayerDemeter>{
+
+    private Ressource toExchange;
+    private Ressource toReceive;
+
+    public ExchangeRessourcesPort( Ressource toexchange , Ressource toreceive ){
+
+        if (toexchange==toreceive){
+            throw new  IllegalArgumentException("2 ressources being exchanged must be different");
+        }
+        this.toExchange=toexchange;
+        this.toReceive=toreceive;
+    }
     // permet d'échanger de ressources identiques identiques contre une autre
     @Override
-    public void act(PlayerDemeter player1 ,PlayerDemeter player2) throws NoMoreRessourcesException {
+    public void act(PlayerDemeter player) throws NoMoreRessourcesException {
+        if (player.getRessourceAmount(toExchange)<2){
+           throw new NoMoreRessourcesException("Not enough Ressources to be able to exchange.")
+        }
+        player.removeRessource(toExchange, 0);
+        player.addRessource(toReceive, 0);
     
 
 
