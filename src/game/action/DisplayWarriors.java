44aa082879c0ca356cd142d6275de22ec74529d4
Almohamed Alsadou Fatima <fatima.almohamed-alsadou.etu@a10p3.fil.univ-lille.fr>
2025-03-13 16:49:19 +0100

    petite modification dans DisplayWarriors

--- src/game/action/DisplayWarriors.java
+++ src/game/action/DisplayWarriors.java
@@ -23,11 +23,11 @@ public class DisplayWarriors implements Action<PlayerAres>{
     public void act(PlayerAres player) throws NoMoreRessourcesException {
         for(int i=0; i<(this.lesjoueurs).length; i++){
 
-            System.out.println("Number Warriors joueur"+this.lesjoueurs[i]+ "est"+ this.lesjoueurs[i].getWarriors());
+            System.out.println("Number Warriors of Player"+this.lesjoueurs[i]+ "est"+ this.lesjoueurs[i].getWarriors());
 
         }
 
-        
+
 
 }
 
