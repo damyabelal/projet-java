f79a936ee8ea17f14ac7704b8d29c715282e5cb7
CHALAH <laeticia.chalah.etu@univ-lille.fr>
2025-03-14 15:41:08 +0100

    ajout de removeFarm()  dans PlayerDemeter

--- src/game/PlayerDemeter.java
+++ src/game/PlayerDemeter.java
@@ -43,26 +43,59 @@ public class PlayerDemeter extends Player{
         return this.nbThief;
     }
 
+    /**
+     * add points to the demeter player
+     * @param nb int number of points to add
+     */
     public void addPoints(int nb){
         this.points += 1;
     }
+    /**
+     * add thief to the  demeter player
+     * @param nb int number of thief to add
+     */
 
     public void addThiefs(int nb){
         this.nbThief += 1;
     }
 
+    /**
+     * get the farms of the demeter player
+     * @return List<Farm> the farms of the demeter player
+     */
     public List<Farm> getFarms(){
         return this.farms;
     }
 
+    /**
+     * add a farm to the demeter player
+     * @param farm the farm to add
+     */
     public void addFarm(Farm farm){
         this.farms.add(farm);
     }
 
+    /**
+     * remove a farm from the demeter player
+     * @param farm the farm to remove
+     */
+    public void removeFarm(Farm farm){
+        this.farms.remove(farm);
+    }
+
+
+    /**
+     * get the exploitations of the demeter player
+     * @return List<Exploitation> the exploitations of the demeter player
+     */
     public List<Exploitation> getExploitations(){
         return this.exploitations;
     }
 
+    /**
+     * add an exploitation to the demeter player
+     * @param exploitation the exploitation to add
+     */
     public void addExploitation(Exploitation exploitation){
         this.exploitations.add(exploitation);
 
