d5f0e22d5767eb331987e3d7f5321602721cd40b
Mille Stella-rose <stellarose.mille.etu@a10p31.fil.univ-lille.fr>
2025-03-14 13:59:59 +0100

    ecriture de l'action d' évolution de l'armée

--- src/game/action/UpgradeArmy.java
+++ src/game/action/UpgradeArmy.java
@@ -2,22 +2,30 @@ package game.action;
 
 import game.NoMoreRessourcesException;
 import game.PlayerAres;
+import game.tuile.Earth;
+import game.tuile.Ressource;
+import game.tuile.building.Camp;
 
-public class UpgradeArmy implements Action<PlayerAres>{
+public class UpgradeArmy extends ActionManager implements Action<PlayerAres>{
 
-    @Override
-    public void act(PlayerAres player) throws NoMoreRessourcesException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'act'");
+    private Earth tuile; 
+
+    public UpgradeArmy(PlayerAres player, Earth tuile){
+        super(player);
+        this.tuile= tuile;
+        this.cost.put(Ressource.WOOD, 2);
+        this.cost.put(Ressource.ORE, 3);
     }
 
-
-
-
-
-    
-
-
+    public void act(PlayerAres player) throws NoMoreRessourcesException {
+        if (!this.hasEnoughRessources()){
+            throw new NoMoreRessourcesException("Not enough ressources to upgrade this army");
+        }
+        this.removeRessources();
+        Camp camp= new Camp(tuile, 0, player); //???
+        tuile.setBuilding(camp);
+        player.addCamp(camp);
+    }
 
     
 }
