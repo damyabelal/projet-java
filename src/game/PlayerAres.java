65bf0e0a7e492071bbfb2b6a0a9d44363eebf499
CHALAH <laeticia.chalah.etu@univ-lille.fr>
2025-03-15 14:16:44 +0100

    ajout de la gestion des actions pour playerAres

--- src/game/PlayerAres.java
+++ src/game/PlayerAres.java
@@ -1,16 +1,29 @@
 package game;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import game.action.Action;
+import game.action.AttackNeighboor;
+import game.action.BuildArmy;
+import game.action.BuySecretWeapon;
+import game.action.BuyWarriors;
+import game.action.UpgradeArmy;
 import game.tuile.building.Army;
 import game.tuile.building.Camp;
+import listchooser.ListChooser;
+
 
 public class  PlayerAres extends Player{
+
 
         private int warriors;
         private int secretWeapon;
         private List<Army> armies;
         private List<Camp> camps;
+        private static ListChooser<Action<PlayerAres>> lc = new ListChooser<>();
+
         
         // initialies a new playerares with 30 warriors , a name and zero secret weapons
         public PlayerAres(String name){
@@ -102,6 +115,56 @@ public class  PlayerAres extends Player{
     public void addCamp(Camp camp){
         this.camps.add(camp);
     }
+
+
+    /**
+     * presents the user with a list of actions that the player can do
+     * @param board the game board
+     */
+    public void act(Board board) throws CantBuildException, NoMoreRessourcesException, IOException{
+        List<Action<PlayerAres>> actions = actionsPlayer(board);
+        Action<PlayerAres> action = lc.choose("Choose an action", actions);
+       
+        if(action != null){
+            try{
+                action.act(this);
+                
+            } catch(NoMoreRessourcesException e){
+                System.out.println(e.getMessage());
+            }
+        }else {
+            System.out.println("No action chosen");
+        }
+           
+    }
+
+
+
+
+
+
+    /**
+     * returns a list of actions that the player Ares can do
+     * @param board
+     * @return List<Action<PlayerAres>>
+     */
+    private List<Action<PlayerAres>> actionsPlayer(Board board){
+        List<Action<PlayerAres>> actions = new ArrayList<>();
+
+        /// on ajoute les actions possibles pour le joueur Ares 
+        actions.add(new BuildArmy(board, this, null));
+        actions.add(new UpgradeArmy(this));
+        actions.add(new AttackNeighboor());
+        actions.add(new BuySecretWeapon(null));
+        actions.add(new BuyWarriors(this));
+        
+        return actions;
+
+    }
+
+
+
+
 
 
 }
