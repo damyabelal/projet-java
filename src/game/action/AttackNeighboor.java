1ca045d6736d77c04c3bf971e247ee16370b238f
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-03-15 15:39:53 +0100

    structure de AttachNeighboor

--- src/game/action/AttackNeighboor.java
+++ src/game/action/AttackNeighboor.java
@@ -1,17 +1,29 @@
 package game.action;
 
+import java.util.List;
+
 import game.NoMoreRessourcesException;
 import game.PlayerAres;
+import game.util.Position;
+import listchooser.ListChooser;
 
-public class AttackNeighboor implements Action<PlayerAres>{
+public class AttackNeighboor extends ActionManager implements Action<PlayerAres>{
 
-    @Override
+    public static ListChooser<Position> lc;
+
+    public AttackNeighboor(PlayerAres player){
+        super(player); 
+    }
+
+    public List<PlayerAres> NeighboorsList(){}
+
+    public PlayerAres askNeighboor(){}
+
+    public Integer howMuchDice(Player player){}
+
     public void act(PlayerAres player) throws NoMoreRessourcesException {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'act'");
-
-
-        
+        throw new UnsupportedOperationException("Unimplemented method 'act'");        
     }
     
 }
