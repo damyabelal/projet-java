18a0b3481e7baf8069573ee56434672df61aa396
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-03-15 14:04:09 +0100

    insertion de listchooser dans BuildArmy

--- src/game/action/BuildPort.java
+++ src/game/action/BuildPort.java
@@ -1,9 +1,14 @@
 package game.action;
+import java.io.IOException;
 import java.util.HashMap;
+
+import game.Board;
 import game.NoMoreRessourcesException;
 import game.Player;
 import game.tuile.Earth;
 import game.tuile.building.Port;
+import game.util.Position;
+import listchooser.ListChooser;
 import game.tuile.Ressource;
 
 /*
@@ -15,13 +20,20 @@ public class BuildPort implements Action<Player>{
 
     private Earth tuile;// the tile that we will place the port on
     protected HashMap<Ressource,Integer> cost;
+    private Board board; 
+    public static ListChooser<Position> lc;
 
-    public BuildPort(Earth tile){
+    public BuildPort(Earth tile, Board board){
         this.tuile=tile;
+        this.board= board; 
         this.cost=new HashMap<>(){ { put(Ressource.WOOD,1);
         put(Ressource.SHEEP,2);
         }};
         }
+
+    public Position askCoordinate() throws IOException {
+        return lc.chooseCoordinate("Where do you want to build a Farm?", this.board);
+    }
     
     @Override
     public void act(Player player) throws NoMoreRessourcesException {
