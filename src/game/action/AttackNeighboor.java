69c60a3c95990ee83d96406ee595b07691ff8493
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-03-15 15:47:34 +0100

    methode pour demander quel voisin on attaque, à voir ce comment on les trouve, si on le fait au préalable ou dans la classe

--- src/game/action/AttackNeighboor.java
+++ src/game/action/AttackNeighboor.java
@@ -3,6 +3,7 @@ package game.action;
 import java.util.List;
 
 import game.NoMoreRessourcesException;
+import game.Player;
 import game.PlayerAres;
 import game.util.Position;
 import listchooser.ListChooser;
@@ -10,16 +11,20 @@ import listchooser.ListChooser;
 public class AttackNeighboor extends ActionManager implements Action<PlayerAres>{
 
     public static ListChooser<Position> lc;
+    public List<PlayerAres> ennemies;
 
-    public AttackNeighboor(PlayerAres player){
+    public AttackNeighboor(PlayerAres player, List<PlayerAres> ennemies){
         super(player); 
+        this.ennemies= ennemies;
     }
 
-    public List<PlayerAres> NeighboorsList(){}
+    public PlayerAres askNeighboor(){
+        return lc.choose("Who de you want to attack", this.ennemies);
+    }
 
-    public PlayerAres askNeighboor(){}
-
-    public Integer howMuchDice(Player player){}
+    public Integer howMuchDice(Player player){
+        return 0;
+    }
 
     public void act(PlayerAres player) throws NoMoreRessourcesException {
         // TODO Auto-generated method stub
