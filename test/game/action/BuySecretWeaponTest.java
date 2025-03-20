bd991cc8dc020177b2ebebfa3b103da3226f99b3
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-03-20 19:35:35 +0100

    BuySecretWeaponTest

--- test/game/action/BuySecretWeaponTest.java
+++ test/game/action/BuySecretWeaponTest.java
@@ -1,8 +1,36 @@
 package game.action;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+
+import game.CantBuildException;
+import game.NoMoreRessourcesException;
+import game.PlayerAres;
+import game.tuile.Ressource;
 
 public class BuySecretWeaponTest{
 
+    private PlayerAres player; 
+    private Action<PlayerAres> action; 
 
+    @BeforeEach
+    void setUp(){
+        player = new PlayerAres("chouette");
+        action= new BuySecretWeapon(player); 
+    }
+
+    @Test
+    void actTest() throws NoMoreRessourcesException, CantBuildException, IOException{
+        assertTrue(player.getNbSecretWeapon()==0);
+        assertThrows(NoMoreRessourcesException.class, () -> action.act(player)); 
+        player.addRessource(Ressource.WOOD, 1);
+        player.addRessource(Ressource.ORE, 1);
+        action.act(player);
+        assertTrue(player.getNbSecretWeapon()==1);
+    }
 
 }
