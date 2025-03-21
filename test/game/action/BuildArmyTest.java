58bdcb484a97daadce50cbc0ebf76fe1dec15f50
Almohamed Alsadou Fatima <fatima.almohamed-alsadou.etu@a10p15.fil.univ-lille.fr>
2025-03-21 14:59:29 +0100

    remplacement de farm par foret

--- test/game/action/BuildArmyTest.java
+++ test/game/action/BuildArmyTest.java
@@ -14,6 +14,7 @@ import game.Board;
 import game.CantBuildException;
 import game.NoMoreRessourcesException;
 import game.PlayerAres;
+import game.tuile.Forest;
 import game.tuile.Sea;
 import game.tuile.building.Farm;
 import game.tuile.building.Port;
@@ -27,10 +28,10 @@ public class BuildArmyTest{
     private Action<PlayerAres> actionBuild; 
     // creation de tuile qui vont servir pour creer une ile de le board parceque comme le board est creer de facon  aleatoire on 
     //veut etre sur que il y a une ile qui respecte les conditions pour poser une armee sur le plateau
-    private Farm farm1;
-    private Farm farm2;
-    private Farm farm3;
-    private Farm farm4;
+    private Forest foret1;
+    private Forest foret2;
+    private Forest foret3;
+ 
     private Sea sea;
     private Port port;
     private Position pos1;
@@ -42,15 +43,18 @@ public class BuildArmyTest{
     @BeforeEach
     void setUp(){
         player = new PlayerAres("Ares");
+
         board= new Board(5, 5); //creation d'un plateau de maniere aleatoire
         action= new BuildArmy(board, player); 
         pos1=new Position(0,0);
         pos2=new Position(1,0);
         pos3=new Position(0,1);
         pos4= new Position(1,1);
+        farm1=new Farm(board.getTile(pos1),player);
+        farm2=new Farm();
         sea= new Sea();
         port =new Port(null, player);
-
+        
         
         actionBuild= new BuildPort(player, board);
 
@@ -59,6 +63,7 @@ public class BuildArmyTest{
 
     @Test
     void BuildTest() throws NoMoreRessourcesException, CantBuildException, IOException{
+        board.put(farm1,pos1)
         assertTrue(player.getArmies().isEmpty());
         actionBuild.act(player);
         actionBuild.act(player);
