eda3b09658f406c122c885e360472b4f8b6d0dd6
fatima.almohamed-alsadou.etu <your_email@example.com>
2025-03-22 13:41:39 +0100

    ajout import dans playThiefTest

--- test/game/action/PlayThiefTest.java
+++ test/game/action/PlayThiefTest.java
@@ -1,5 +1,16 @@
 package game.action;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+
+import game.CantBuildException;
+import game.NoMoreRessourcesException;
+import game.PlayerAres;
+import game.tuile.Ressource;
 
 public class PlayThiefTest{
 
