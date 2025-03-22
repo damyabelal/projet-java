ff32c2d7a8c4a41e02bf35028d2c96724be9b9a7
fatima.almohamed-alsadou.etu <your_email@example.com>
2025-03-22 13:46:21 +0100

    creation d'un board dans thieftest

--- test/game/action/PlayThiefTest.java
+++ test/game/action/PlayThiefTest.java
@@ -7,12 +7,27 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
 
+import game.Board;
 import game.CantBuildException;
 import game.NoMoreRessourcesException;
 import game.PlayerAres;
+import game.PlayerDemeter;
 import game.tuile.Ressource;
 
 public class PlayThiefTest{
+  // creating 2 players so that player1 could steal the ressources of the second player
+  private PlayerDemeter player1;
+  private PlayerDemeter player2;
+  private Board board;
 
-    
+  
+
+  void setUp(){
+    player1= new PlayerDemeter("lucie");
+    player2=new PlayerDemeter("luca");
+    board=new Board(5,5);
+
+
+  }  
+
 }
