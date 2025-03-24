1f1719c61fc68e5396d34e01fe3d9e3f321f7955
Almohamed Alsadou Fatima <fatima.almohamed-alsadou.etu@a16p13.fil.univ-lille.fr>
2025-03-24 11:04:31 +0100

    modification javadoc upgradeFarm

--- src/game/action/BuySecretWeapon.java
+++ src/game/action/BuySecretWeapon.java
@@ -5,7 +5,7 @@ import game.tuile.Ressource;
 import game.NoMoreRessourcesException;
 
 /*
- * This class is used to buy a secret weapon
+ * buys a secret weapon
  * it extends ActionManager and implements Action<PlayerAres>
  */
 public class BuySecretWeapon extends ActionManager implements Action<PlayerAres>{
@@ -17,20 +17,19 @@ public class BuySecretWeapon extends ActionManager implements Action<PlayerAres>
         this.cost.put(Ressource.WOOD, 1);
     }
     /**
-     * buy a secret weapon if the player have enough ressources
-     * @param player the player who make the action
-     * @throws NoMoreRessourcesException if the player doesn't have enough ressources to
-     * buy
+     * buys a secret weapon if the player has enough ressources for it
+     * @param player the player who wants to buy
+     * @throws NoMoreRessourcesException if the player doesn't have enough ressources to buy
      */
     @Override
     public void act(PlayerAres player) throws NoMoreRessourcesException {
 
-        // we check if the player has enough ressources to buy a secret weapon
+        // checks if the player has enough ressources to buy a secret weapon
         if (! this.hasEnoughRessources() ){
             throw new NoMoreRessourcesException("Not enough ressources to buy a secret weapon");
         }
         this.removeRessources();
-        // we add a secret weapon to the player's weapons stock
+        // adds a secret weapon to the player's weapons stock
         player.addSecretWeapon();
         System.out.println(player.getName()+ " "+ player.getResources()+" (" +player.getWarriors()+ " warriors) now have a secret weapon");
     }
