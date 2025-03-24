f6a3e04979471e76566fb4639efaea8b42618e12
Almohamed Alsadou Fatima <fatima.almohamed-alsadou.etu@a16p13.fil.univ-lille.fr>
2025-03-24 11:36:48 +0100

    ajout commentaire dans BuildFarmTest

--- test/game/action/BuildFarmTest.java
+++ test/game/action/BuildFarmTest.java
@@ -29,11 +29,15 @@ public class BuildFarmTest{
 
     @Test
     void BuildTest() throws NoMoreRessourcesException, CantBuildException, IOException {
+        // at the beginning , the player has no farms
         assertTrue(player.getFarms().isEmpty());
+        // the player has no ressources , so if he attemps to build a farm it should throw an exception
         assertThrows(NoMoreRessourcesException.class, () -> action.act(player)); 
+        // adding enough ressources so the player could build a farm
         player.addRessource(Ressource.WOOD, 1);
         player.addRessource(Ressource.ORE, 1);
         action.act(player);
+        // after building a farm , the player should have a farm in his inventory
         assertTrue(! player.getFarms().isEmpty());
     }
 
