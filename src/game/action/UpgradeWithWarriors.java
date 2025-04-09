53f416953388d4545e5c7fb42e3d4ed69faf2f34
fatima.almohamed-alsadou.etu <your_email@example.com>
2025-04-09 12:22:58 +0200

    modif constructeur de  upgradeWithWarriors et methode askWarriors

--- src/game/action/UpgradeWithWarriors.java
+++ src/game/action/UpgradeWithWarriors.java
@@ -5,6 +5,8 @@ import game.Player;
 import game.PlayerAres;
 import game.listchooser.RandomListChooser;
 import game.tuile.building.Army;
+
+import java.io.IOException;
 import java.util.List;
 import game.CantBuildException;
 import game.tuile.Earth;
@@ -19,15 +21,20 @@ public class UpgradeWithWarriors extends ActionManager implements Action<PlayerA
   private RandomListChooser lc;
 
   // army is the army that the given player player wants to upgrade
-  public UpgradeWithWarriors(Player player,Army army) {
+  public UpgradeWithWarriors(Player player) {
     super(player);
-    this.army=army;
     this.cost.put(Ressource.WOOD, 2);
     this.cost.put(Ressource.ORE, 3);
     this.lString = new RandomListChooser<>();
     this.lnumb = new RandomListChooser<>();
     this.lc = new RandomListChooser<>();
    
+  }
+
+  @Override
+  public void act(PlayerAres player) throws NoMoreRessourcesException, CantBuildException, IOException {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'act'");
   }
 
 }
