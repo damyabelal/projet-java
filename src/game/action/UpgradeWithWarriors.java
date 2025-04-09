9da216b9432462427da37fe7a0f450f106ff1273
fatima.almohamed-alsadou.etu <your_email@example.com>
2025-04-09 12:33:28 +0200

    fin methode acte de updrageWithWarriors

--- src/game/action/UpgradeWithWarriors.java
+++ src/game/action/UpgradeWithWarriors.java
@@ -66,34 +66,24 @@ public class UpgradeWithWarriors extends ActionManager implements Action<PlayerA
     Army chosenArmy = askArmy();
 
     // asks the player how he wants to upgrade the army 
-    String method = askUpgradeMethod();
+ 
 
     // checks if the player has  an army to upgrade
     if (chosenArmy == null) {
         throw new IllegalArgumentException("No army selected");
     }
-    // checks if the player has enough ressources
-    if ("resources".equalsIgnoreCase(method)) {
-        if (!this.hasEnoughRessources()) {
-            throw new NoMoreRessourcesException("Not enough resources to upgrade the army");
-        }
-        this.removeRessources();
-
-    } else if ("warriors".equalsIgnoreCase(method)) {
-        // check if the army has 5 warriors to upgrade
-        if (chosenArmy.getNbWarriors() < 5) {
+    
+  // check if the army has 5 warriors to upgrade
+  if (chosenArmy.getNbWarriors() < 5) {
             throw new CantBuildException("To upgrade an army to a camp, the army must have 5 warriors");
         }
-        int add = askNumberOfWarriors();
-        if (player.getWarriors() < add){
-            throw new CantBuildException("To upgrade an army to a camp, you must have enough warriors in stock");
-         }
-        chosenArmy.addWarriors(add);
-        player.removeWarriors(add);
+   int add = askNumberOfWarriors();
+  if (player.getWarriors() < add){
+  throw new CantBuildException("To upgrade an army to a camp, you must have enough warriors in stock");
+  }
+  chosenArmy.addWarriors(add);
+  player.removeWarriors(add);
 
-    } else {
-        throw new IllegalArgumentException("Invalid upgrade method");
-    }
 
     this.tuile = chosenArmy.getTuile(); 
 
