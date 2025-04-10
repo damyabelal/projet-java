b8cbd2263a40256027fdd622253cf639faf1d0dd
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-04-10 23:07:09 +0200

    creation nouveau main

--- src/game/Demeter.java
+++ src/game/Demeter.java
@@ -1,5 +1,28 @@
 package game;
 
 public class Demeter {
+
+    public static void main(String[] args){
+        if (args.length < 3) {
+            System.out.println("You have to give two positive settings and the number of player.");
+            return;
+        }
+        int width = Integer.parseInt(args[0]);
+        int height = Integer.parseInt(args[1]);
+
+        if (width < 10 || height < 10) {
+            System.out.println("minimum dimensions must be 10");
+            return;
+        }
+        System.out.println("---------------------");
+        System.out.println("-------DEMETER-------");
+        System.out.println("---------------------");
+        System.out.println("\n"); 
+
+        Board board = new Board(width, height);
+
+
+    }
+    
     
 }
