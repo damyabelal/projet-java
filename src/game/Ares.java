668c713319dd4b22e9c460bec06504af5b83703e
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-04-12 18:23:28 +0200

    squelette ares

--- src/game/Ares.java
+++ src/game/Ares.java
@@ -1,13 +1,15 @@
 package game;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import game.listchooser.util.Input;
+import game.util.InvalidChoiceException;
 
 public class Ares {
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, InvalidChoiceException{
         if (args.length < 3) {
             System.out.println("You have to give two positive settings and the number of player.");
             return;
@@ -39,17 +41,34 @@ public class Ares {
 
         //création des différents joueurs
         for (int i=0; i<nbPlayer; i++){
+            System.out.println("j"+(i+1)+" name:");
             String name= Input.readString(); 
             players.add(new PlayerAres(name)); 
         }
 
+        for (PlayerAres p: players){
+            p.createActions(board, 0);
+        }
+
         while (winner==null){
             for (PlayerAres p: players){
-                
+                System.out.println(p.getName()+" (" +p.getResources()+ ") turn!!"); 
+                p.collectRessources();
+                board.display();
+                p.act(board, 0);
+                if( isWinner(p)){
+                    winner= p; 
+                    break; 
+                }
             }
         }
 
 
+    }
+
+    private static boolean isWinner(PlayerAres player){
+        //retourne si le joueur as gagné ou pas, selon l'objectif déterminer en d"but de jeu
+        return false; 
     }
 
 }
