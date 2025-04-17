f4d2bab86eaac1de2532040e87ececdec1b0cfa2
fatima.almohamed-alsadou.etu <your_email@example.com>
2025-04-17 08:53:07 +0200

    petite modif dans aresgameojectives et fin premiere methode de test dans aresGameObjectives

--- test/game/AresGameObjectivesTest.java
+++ test/game/AresGameObjectivesTest.java
@@ -1,5 +1,7 @@
 package game;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
 
 import game.AresGameObjectives.ObjectiveType;
@@ -10,13 +12,32 @@ public class AresGameObjectivesTest{
  void setUp() {
   AresGameObjectives conquerir =new AresGameObjectives(ObjectiveType.CONQUER_TILES, 0);
   AresGameObjectives envahir =new AresGameObjectives(ObjectiveType.EVADE_ISLANDS, 0);
-  AresGameObjectives envahir =new AresGameObjectives(ObjectiveType.DETAIN_WARRIORS, 0);
+  AresGameObjectives detenir =new AresGameObjectives(ObjectiveType.DETAIN_WARRIORS, 0);
   
 
  }
 
  @Test
  public void  ObjectiveGetTypeTest(){
+  AresGameObjectives objective = new AresGameObjectives(
+            ObjectiveType.CONQUER_TILES, 10
+        );
+        assertEquals(ObjectiveType.CONQUER_TILES, objective.getType());
+
+
+ AresGameObjectives objective2 = new AresGameObjectives(
+          ObjectiveType.DETAIN_WARRIORS, 10
+      );
+      assertEquals(ObjectiveType.DETAIN_WARRIORS, objective2.getType());
+
+
+
+      AresGameObjectives objective3 = new AresGameObjectives(
+        ObjectiveType.INVADE_ISLANDS, 10
+    );
+    assertEquals(ObjectiveType.DETAIN_WARRIORS, objective3.getType());
+
+
 
 
  }
