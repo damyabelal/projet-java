ad69981aa88da25447160c62bc558b6a37674758
fatima.almohamed-alsadou.etu <your_email@example.com>
2025-04-17 08:49:24 +0200

    methode de test objectivegettypetest de aresGameobjectivesTest en cours

---
+++ test/game/AresGameObjectivesTest.java
@@ -0,0 +1,26 @@
+package game;
+
+import org.junit.Test;
+
+import game.AresGameObjectives.ObjectiveType;
+
+public class AresGameObjectivesTest{
+
+
+ void setUp() {
+  AresGameObjectives conquerir =new AresGameObjectives(ObjectiveType.CONQUER_TILES, 0);
+  AresGameObjectives envahir =new AresGameObjectives(ObjectiveType.EVADE_ISLANDS, 0);
+  AresGameObjectives envahir =new AresGameObjectives(ObjectiveType.DETAIN_WARRIORS, 0);
+  
+
+ }
+
+ @Test
+ public void  ObjectiveGetTypeTest(){
+
+
+ }
+
+
+
+}
