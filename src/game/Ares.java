51800bda4fe87066135baebf5b529f8de1d79d95
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-19 22:03:12 +0200

    ajout d'un try catch pour gérer les cas des NullPointerException dans ares

--- src/game/Ares.java
+++ src/game/Ares.java
@@ -12,8 +12,9 @@ import game.util.InvalidChoiceException;
 import game.util.NoMoreRessourcesException;
 
 public class Ares {
-    
-    public static void main(String[] args) throws IOException, InvalidChoiceException, CantBuildException, NoMoreRessourcesException{
+
+    public static void main(String[] args)
+            throws IOException, InvalidChoiceException, CantBuildException, NoMoreRessourcesException {
         if (args.length < 3) {
             System.out.println("You have to give two positive settings and the number of player.");
             return;
@@ -21,14 +22,14 @@ public class Ares {
 
         int width = Integer.parseInt(args[0]);
         int height = Integer.parseInt(args[1]);
-        int nbPlayer= Integer.parseInt(args[2]); 
+        int nbPlayer = Integer.parseInt(args[2]);
 
         if (width < 10 || height < 10) {
             System.out.println("minimum dimensions must be 10");
             return;
         }
 
-        if (nbPlayer < 2 ) {
+        if (nbPlayer < 2) {
             System.out.println("You should play with at least two player");
             return;
         }
@@ -36,75 +37,81 @@ public class Ares {
         System.out.println("--------------------");
         System.out.println("--------ARES--------");
         System.out.println("--------------------");
-        System.out.println("\n"); 
+        System.out.println("\n");
 
         Board board = new Board(width, height);
 
-        List<PlayerAres> players= new ArrayList<>(); 
-        PlayerAres winner= null; 
+        List<PlayerAres> players = new ArrayList<>();
+        PlayerAres winner = null;
 
-        //création des différents joueurs
-        for (int i=0; i<nbPlayer; i++){
-            System.out.println("j"+(i+1)+" name:");
-            String name= Input.readString(); 
+        // création des différents joueurs
+        for (int i = 0; i < nbPlayer; i++) {
+            System.out.println("j" + (i + 1) + " name:");
+            String name = Input.readString();
             players.add(new PlayerAres(name));
-             
+
         }
 
         // tir au sort un objectif pour chaque joueuer
         System.out.println("\n");
         System.out.println("Player's goals");
-        for (PlayerAres p:players){
+        for (PlayerAres p : players) {
             p.givePlayersObjective();
-            System.out.println(p.getName() + " goal is to "+ p.getObjective() );
+            System.out.println(p.getName() + " goal is to " + p.getObjective());
         }
 
-        for (PlayerAres p: players){
+        for (PlayerAres p : players) {
             p.createActions(board, 0);
         }
 
-
-        //ici on vas laisser les joueurs construire leurs deux armées 
+        // ici on vas laisser les joueurs construire leurs deux armées
         System.out.println("\n");
         System.out.println("TIME TO BUILD YOUR ARMIES !");
         System.out.println("\n");
 
-
-         // premier tour : ordre croissant
-         for (int i=0 ;i<players.size();i++){
-            System.out.println(players.get(i).getName()+" place your first initial farm: ");
-            players.get(i).placeInitialArmy(board, new InteractiveListChooser<Earth>(), new InteractiveListChooser<Integer>()); 
+        // premier tour : ordre croissant
+        for (int i = 0; i < players.size(); i++) {
+            System.out.println(players.get(i).getName() + " place your first initial farm: ");
+            players.get(i).placeInitialArmy(board, new InteractiveListChooser<Earth>(),
+                    new InteractiveListChooser<Integer>());
         }
         // deuxieme tour : ordre decroissant
-        for (int i=players.size()-1 ;i>=0;i--){
-            System.out.println(players.get(i).getName()+" (" +players.get(i).getResources()+"/ nb warriors: "+ players.get(i).getWarriors() + ") build your second army : ");
-            players.get(i).placeInitialArmy(board, new InteractiveListChooser<Earth>(), new InteractiveListChooser<Integer>()); 
+        for (int i = players.size() - 1; i >= 0; i--) {
+            System.out.println(players.get(i).getName() + " (" + players.get(i).getResources() + "/ nb warriors: "
+                    + players.get(i).getWarriors() + ") build your second army : ");
+            players.get(i).placeInitialArmy(board, new InteractiveListChooser<Earth>(),
+                    new InteractiveListChooser<Integer>());
         }
-
 
         System.out.println("\n");
         System.out.println("THE GAME START");
         System.out.println("\n");
-        int i= 0; //pour compter le nombre de tour
-        while (winner==null){
-            System.out.println("ROUND "+ i );
-            for (PlayerAres p: players){
-                System.out.println(p.getName()+" (" +p.getResources()+"/ nb warriors: "+ p.getWarriors() + ") turn!!"); 
+        int i = 0; // pour compter le nombre de tour
+        while (winner == null) {
+            System.out.println("ROUND " + i);
+            for (PlayerAres p : players) {
+                System.out.println(
+                        p.getName() + " (" + p.getResources() + "/ nb warriors: " + p.getWarriors() + ") turn!!");
                 p.collectRessources();
                 board.display();
                 p.createActions(board, 0);// changer la liste des actions a chaque tour
-                p.act(board, 0);
-                if( isWinner(p, board)){
-                    winner= p; 
-                    break; 
+                try {
+                    p.act(board, 1);
+                } catch (NullPointerException e) {
+                    System.out.println("Action cancelled due to invalid or null choice.");
+
+                }
+                if (isWinner(p, board)) {
+                    winner = p;
+                    break;
                 }
             }
-            i++; 
+            i++;
             System.out.println("\n");
         }
     }
 
-    private static boolean isWinner(PlayerAres player, Board board){
+    private static boolean isWinner(PlayerAres player, Board board) {
         return player.isObjectiveAchieved(board);
     }
 
--- src/game/AresRandom.java
+++ src/game/AresRandom.java
@@ -10,11 +10,11 @@ import game.util.CantBuildException;
 import game.util.InvalidChoiceException;
 import game.util.NoMoreRessourcesException;
 
+public class AresRandom {
 
-public class AresRandom{
+  public static void main(String[] args)
+      throws IOException, InvalidChoiceException, CantBuildException, NoMoreRessourcesException {
 
-  public static void main(String[] args) throws  IOException, InvalidChoiceException ,CantBuildException, NoMoreRessourcesException{
-    
     if (args.length < 3) {
       System.out.println("You have to give two positive settings and the number of player.");
       return;
@@ -24,12 +24,12 @@ public class AresRandom{
     int height = Integer.parseInt(args[1]);
     int nbplayer = Integer.parseInt(args[2]);
 
-    if(width <10 || height<10){
+    if (width < 10 || height < 10) {
       System.out.println("Width and height must be greater than 10.");
       return;
     }
 
-    if (nbplayer < 2 ) {
+    if (nbplayer < 2) {
       System.out.println("Number of players must be between 2 .");
       return;
     }
@@ -38,36 +38,32 @@ public class AresRandom{
     System.out.println("------ARES RANDOM---");
     System.out.println("--------------------");
 
-
     Board board = new Board(width, height);
-    
-    
-    List<PlayerAres> players= new ArrayList<>(); 
-    PlayerAres winner= null; 
 
-    //création des différents joueurs
-    for (int i=0; i<nbplayer; i++){
-        String name= "j"+(i+1); 
-        players.add(new PlayerAres(name)); 
+    List<PlayerAres> players = new ArrayList<>();
+    PlayerAres winner = null;
+
+    // création des différents joueurs
+    for (int i = 0; i < nbplayer; i++) {
+      String name = "j" + (i + 1);
+      players.add(new PlayerAres(name));
     }
 
-
-    //Placement initail des armees 
+    // Placement initail des armees
     System.out.println("Placement initial des armées (mode aléatoire) :");
 
-
     System.out.println("\n");
-        System.out.println("TIME TO BUILD YOUR ARMIES !");
-        System.out.println("\n");
-        for (int j= 0; j<2; j++){
-            for( PlayerAres p: players){
-                System.out.println(p.getName()+" (" +p.getResources()+"/ nb warriors: "+ p.getWarriors() + ") build your armies");
-                p.placeInitialArmy(board, new RandomListChooser<Earth>(), new RandomListChooser<Integer>()); 
-            }
-        }
+    System.out.println("TIME TO BUILD YOUR ARMIES !");
+    System.out.println("\n");
+    for (int j = 0; j < 2; j++) {
+      for (PlayerAres p : players) {
+        System.out.println(
+            p.getName() + " (" + p.getResources() + "/ nb warriors: " + p.getWarriors() + ") build your armies");
+        p.placeInitialArmy(board, new RandomListChooser<Earth>(), new RandomListChooser<Integer>());
+      }
+    }
 
-
-    //initialisation des actions
+    // initialisation des actions
     for (PlayerAres p : players) {
       p.createActions(board, 1);
     }
@@ -79,18 +75,19 @@ public class AresRandom{
         board.display();
 
         p.createActions(board, 1);
-        p.act(board, 1);
+        try {
+          p.act(board, 1);
+        } catch (NullPointerException e) {
+          System.out.println("Action cancelled due to invalid or null choice.");
+
+        }
         if (p.isObjectiveAchieved(board)) {
           winner = p;
           break;
-          }
+        }
       }
-  }
-    
-    
-  }
+    }
 
-  
-
+  }
 
 }
