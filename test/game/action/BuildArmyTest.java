0dba9495e707ec0dc15ff3d489981c6e58fbbc71
STELLA ROSE MILLE <stellarosemille@MacBook-Air-de-Marie-Helene.local>
2025-04-19 18:46:33 +0200

    j'ai juste ajouté une declaration

--- test/game/action/BuildArmyTest.java
+++ test/game/action/BuildArmyTest.java
@@ -96,7 +96,7 @@ public class BuildArmyTest {
     }
 
     @Test
-    void canBuildArmyTest() {
+    void canBuildArmyTest() throws NoMoreRessourcesException {
         board = new Board(5, 5);
         for (int x = 0; x < 5; x++) {
             for (int y = 0; y < 5; y++) {
