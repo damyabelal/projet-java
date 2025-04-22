fb41ccf8969b9790895bfb6849faacc6db27a3b7
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-22 10:06:20 +0200

    ajout des autres methodes de tests pour fixedIndex

--- test/game/listchooser/FixedIndexChooserTest.java
+++ test/game/listchooser/FixedIndexChooserTest.java
@@ -15,5 +15,17 @@ class FixedIndexChooserTest {
         assertEquals("army2", chooser.choose("choose your army", armies));
     }
 
-   
+    @Test
+    void testChooseEmptyArmyList() {
+        FixedIndexChooser<String> chooser = new FixedIndexChooser<>(0);
+        List<String> armies = List.of();
+        assertNull(chooser.choose("choose your army", armies));
+    }
+
+    @Test
+    void testChooseArmyIndexOutOfBounds() {
+        FixedIndexChooser<String> chooser = new FixedIndexChooser<>(3);
+        List<String> armies = List.of("army1", "army2", "army3");
+        assertThrows(IndexOutOfBoundsException.class, () -> chooser.choose("choose your army", armies));
+    }
 }
